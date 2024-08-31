6814e8b26fce21da140e36f46e75352b92ce0093
zbonneau <zbonneau@ttu.edu>
2024-08-31 12:35:58 -0500

    Code cited, commented

--- IndividualProject/roboflow.py
+++ IndividualProject/roboflow.py
@@ -3,14 +3,15 @@ import numpy as np
 from inference import get_model
 import supervision as sv
 
-
+# Open Camera
 camera: cv2.VideoCapture = cv2.VideoCapture(0)
 model = get_model(model_id="playing-cards-ow27d/4", api_key="------NO SIR YOU GET NONE OF THIS-----")
 
-
+# Check for success
 if (not camera.isOpened()):
     exit()
 
+# Open Video File
 width = int(camera.get(3))
 height = int(camera.get(4))
 recording = cv2.VideoWriter("DetectPlayingCards.avi",
@@ -18,23 +19,28 @@ recording = cv2.VideoWriter("DetectPlayingCards.avi",
                             20, (width, height))
 
 while (1):
+    # Read frame
     _, frame = camera.read()
 
-    #print(np.shape(frame))
+    """ *****************************************************
+    Title:  Run a Private, Fine-Tuned Model
+    Author: Roboflow
+    Date:   n.d. (Accessed Aug 31 2024)
+    Code Version: No version
+    Availability: https://inference.roboflow.com/quickstart/explore_models/#run-a-private-fine-tuned-model 
+        *****************************************************"""
+    
+    # Execute model on frame
     results = model.infer(frame)[0]
     detections = sv.Detections.from_inference(results)
     boxer = sv.BoundingBoxAnnotator()
     noter = sv.LabelAnnotator()
-    annotated_image = boxer.annotate(
-        scene=frame, detections=detections
-    )
-    annotated_image = noter.annotate(
-        scene=annotated_image, detections=detections
-    )
-
-
-
     
+    # Draw results on frame
+    annotated_image = boxer.annotate(scene=frame, detections=detections)
+    annotated_image = noter.annotate(scene=annotated_image, detections=detections)
+
+    # Display frame, save video
     cv2.imshow("Detected Playing Cards", annotated_image)
     recording.write(annotated_image)
     char = (cv2.waitKey(1) & 0xFF)
