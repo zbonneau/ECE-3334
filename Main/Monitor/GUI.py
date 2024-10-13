c1d5c575e69d5401eca41f43a961f973476a5a7b
zbonneau <zbonneau@ttu.edu>
2024-10-13 14:41:38 -0500

    GUI has multiple tabs for data view and configuration. Config buttons have no functionality yet

--- Main/Monitor/GUI.py
+++ Main/Monitor/GUI.py
@@ -1,7 +1,7 @@
 import sys
 import sqlite3 as sql
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QGridLayout, QWidget, QPushButton, QLineEdit, QLabel, QSpacerItem
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QGridLayout, QWidget, QPushButton, QLineEdit, QLabel, QTabWidget
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.ticker import MaxNLocator
@@ -27,18 +27,18 @@ class PlotCanvas(FigureCanvas):
         self.ax.set_ylabel("Signal Measurements")
         self.ax.set_title(title)
         self.ax.xaxis.set_major_locator(MaxNLocator(nbins=10))
-        self.ax.set_xticklabels(x, rotation=30, ha= 'right')
+        for label in self.ax.get_xticklabels():
+            label.set_rotation(30)
+            label.set_ha('right')
         self.ax.legend(y.columns)
 
         self.fig.tight_layout()
         self.draw()
 
-
-class App(QMainWindow):
+class DataViewTab(QWidget):
     def __init__(self) -> None:
         super().__init__()
-        self.setWindowTitle("View Greenhouse Signals")
-        self.setGeometry(100,100,800,800)
+        # self.tabLabel = QLabel("View GreenHouse Data")
         self.canvas = PlotCanvas(self)
         self.canvas.setSizeIncrement(0,100)
         self.draw_button = QPushButton("View TimeFrame")
@@ -62,13 +62,13 @@ class App(QMainWindow):
         grid.addWidget(self.EndLabel,    3, 0, alignment=Qt.AlignmentFlag.AlignRight)
         grid.addWidget(self.dtEnd,       3, 1)
 
+        # layout.addWidget(self.tabLabel)
         layout.addLayout(grid)
         layout.addWidget(self.draw_button)
         layout.addWidget(self.canvas)
 
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
+        self.setLayout(layout)
+        
 
     def viewData(self)->None:
         path = "Main\\Monitor\\test.db"
@@ -82,6 +82,84 @@ class App(QMainWindow):
         con.close()
 
         self.canvas.plot(df, f"House: {self.houseSelect.text()} Date/Time: {self.dtStart.text()} -- {self.dtEnd.text()}")
+
+class HouseConfigTab(QWidget):
+    def __init__(self)->None:
+        super().__init__()
+        # self.tabLabel = QLabel("Configure House Parameters")
+        self.getCurrent = QPushButton("Get Current Parameters")
+        self.getCurrent.clicked.connect(self.getCurrentParams)
+        self.setNew  = QPushButton("Set House Parameters")
+        self.setNew.clicked.connect(self.setParams)
+        self.houseLabel = QLabel("Select House")
+        self.houseValue = QLineEdit("1")
+
+        self.tempMinLabel = QLabel("Temp Min")
+        self.tempMinValue = QLineEdit()
+        self.tempMaxLabel = QLabel("Temp Max")
+        self.tempMaxValue = QLineEdit()
+        
+        self.HumdMinLabel = QLabel("Humidity Min")
+        self.HumdMinValue = QLineEdit()
+        self.HumdMaxLabel = QLabel("Humidity Max")
+        self.HumdMaxValue = QLineEdit()
+        
+        self.MoistMinLabel = QLabel("Soil Moisture Min")
+        self.MoistMinValue = QLineEdit()
+        self.MoistMaxLabel = QLabel("Soil Moisture Max")
+        self.MoistMaxValue = QLineEdit()
+
+        self.configureLabel =  QLabel("Config Results: ")
+        self.configureResult = QLabel("")
+
+        layout = QVBoxLayout()
+        grid = QGridLayout()
+        grid.setSpacing(5)
+        grid.addWidget(self.houseLabel, 0,0, alignment=Qt.AlignmentFlag.AlignRight)
+        grid.addWidget(self.houseValue, 0, 1)
+        grid.addWidget(self.tempMinLabel, 1, 0, alignment=Qt.AlignmentFlag.AlignRight)
+        grid.addWidget(self.tempMinValue, 1, 1)
+        grid.addWidget(self.tempMaxLabel, 1, 2, alignment=Qt.AlignmentFlag.AlignRight)
+        grid.addWidget(self.tempMaxValue, 1, 3)
+        grid.addWidget(self.HumdMinLabel, 2, 0, alignment=Qt.AlignmentFlag.AlignRight)
+        grid.addWidget(self.HumdMinValue, 2, 1)
+        grid.addWidget(self.HumdMaxLabel, 2, 2, alignment=Qt.AlignmentFlag.AlignRight)
+        grid.addWidget(self.HumdMaxValue, 2, 3)
+        grid.addWidget(self.MoistMinLabel, 3, 0, alignment=Qt.AlignmentFlag.AlignRight)
+        grid.addWidget(self.MoistMinValue, 3, 1)
+        grid.addWidget(self.MoistMaxLabel, 3, 2, alignment=Qt.AlignmentFlag.AlignRight)
+        grid.addWidget(self.MoistMaxValue, 3, 3)
+        grid.addWidget(self.setNew, 4,0, 1, 4)
+        grid.addWidget(self.configureLabel, 5, 0, alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignRight)
+        grid.addWidget(self.configureResult, 5, 1, 1, 3, alignment = Qt.AlignmentFlag.AlignTop)
+
+        # layout.addWidget(self.tabLabel)
+        layout.addWidget(self.getCurrent)
+        layout.addLayout(grid)
+        # layout.addWidget(self.setNew)
+        # layout.addWidget(self.configureResult)
+
+        self.setLayout(layout)
+        
+        
+
+    def getCurrentParams(self)->None:
+        pass
+
+    def setParams(self)->None:
+        pass
+
+
+class App(QMainWindow):
+    def __init__(self)->None:
+        super().__init__()
+        self.setWindowTitle("Automated Greenhouse")
+        self.setGeometry(100,100,800,800)
+
+        container = QTabWidget()
+        container.addTab(DataViewTab(), "View Data")
+        container.addTab(HouseConfigTab(), "Config House")
+        self.setCentralWidget(container)
 
 
 if __name__ == "__main__":
