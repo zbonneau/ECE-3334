841a1153202e057049cd602f76b9538270abd233
zbonneau <zbonneau@ttu.edu>
2024-11-17 00:08:40 -0600

    Commented database manip for socket only test

--- Main/ControllerTest.py
+++ Main/ControllerTest.py
@@ -33,39 +33,41 @@ if __name__ == "__main__":
     server = socket()
     server.bind(('0.0.0.0', 5000))
 
-    con = sql.Connection("Controller\\test.db")
-    con.execute("""
-            CREATE TABLE IF NOT EXISTS data(
-            TIMESTAMP TEXT,
-            HOUSEID   INTEGER,                          
-            TEMP      REAL,
-            HUMIDITY  REAL,
-            MOISTURE  REAL
-            );
-            """ )
-    paramss = [
-        ('2024-10-10 10:00', 1, 10, 20, 20),
-        ('2024-10-10 10:15', 1, 11, 21, 22),
-        ('2024-10-10 10:30', 1, 12, 22, 23),
-        ('2024-10-10 10:45', 1, 13, 23, 24),
-        ('2024-10-10 11:00', 1, 14, 24, 25),
-        ('2024-10-10 10:15', 1, 15, 25, 26)
-    ]
-    for params in paramss:
-        con.execute("INSERT INTO data VALUES(?,?,?,?,?);", params)
-    con.commit()
-    con.close()
+    # con = sql.Connection("Controller\\test.db")
+    # con.execute("""
+    #         CREATE TABLE IF NOT EXISTS data(
+    #         TIMESTAMP TEXT,
+    #         HOUSEID   INTEGER,                          
+    #         TEMP      REAL,
+    #         HUMIDITY  REAL,
+    #         MOISTURE  REAL
+    #         );
+    #         """ )
+    # paramss = [
+    #     ('2024-10-10 10:00', 1, 10, 20, 20),
+    #     ('2024-10-10 10:15', 1, 11, 21, 22),
+    #     ('2024-10-10 10:30', 1, 12, 22, 23),
+    #     ('2024-10-10 10:45', 1, 13, 23, 24),
+    #     ('2024-10-10 11:00', 1, 14, 24, 25),
+    #     ('2024-10-10 10:15', 1, 15, 25, 26)
+    # ]
+    # for params in paramss:
+    #     con.execute("INSERT INTO data VALUES(?,?,?,?,?);", params)
+    # con.commit()
+    # con.close()
 
     server.settimeout(10)
     try:
         while True:
             try:
                 server.listen(1)
+                con,addr = server.accept()
             
             except TimeoutError:
                 continue
 
-            con,addr = server.accept()
+            print(f'Connected pi@{addr[0]}')
+
             task = Thread(target=Listen, args=(con,addr))
             task.start()
 
