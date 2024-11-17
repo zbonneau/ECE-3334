f7794ca2c51a596b2141d8c0f7cea100e84099e1
zbonneau <zbonneau@ttu.edu>
2024-11-17 00:10:21 -0600

    closeApplication tells threads to close safely. Critical for greenhouse_main, less so for comms.

--- Main/Controller/ControllerTop.py
+++ Main/Controller/ControllerTop.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from greenhouse_main import main
+from greenhouse_main import main as greenhouseMain
 from ControllerComms import clientThread
 from globals import glo
 
@@ -7,16 +7,22 @@ from globals import glo
 
 def startup()->None:
     comms: Thread = Thread(target=clientThread)
-    main: Thread  = Thread(target=main)
+    main: Thread  = Thread(target=greenhouseMain)
 
     comms.start()
     main.start()
     try:
-        while True:
-            pass
-    except Exception:
-        comms.join()
+        while not glo.closeApplication:
+            continue
+    except KeyboardInterrupt:
+        print("User-Event for Shutdown")
+    except Exception as error:
+        print(f"Top: {error.__str__()}")
+
+    finally:
+        glo.closeApplication = True # Tell threads to close. may take Up to "PollInterval" minutes to execute 
         main.join()
+        comms.join()
 
 
 if __name__ == "__main__":
